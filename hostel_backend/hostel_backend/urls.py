1cfe88f9bad2daedf8848597d72685f8ba06d008
Tharun Lokesh uggina <tharunlokeshu@gmail.com>
2025-07-31 18:31:19 +0530

    Update urls.py

--- hostel_backend/hostel_backend/urls.py
+++ hostel_backend/hostel_backend/urls.py
@@ -4,7 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
+from django.contrib import admin
+from django.urls import path, include
+from django.http import JsonResponse
+# 👇 Temporary home view to test Render deployment
+def home(request):
+    return JsonResponse({"message": "Hostel IQ Backend is Live 🔥"})
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),          # your user app
@@ -12,6 +17,10 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/users/', include('users.urls')),
+    path('', home),
 ]
+
+
+
 
 
